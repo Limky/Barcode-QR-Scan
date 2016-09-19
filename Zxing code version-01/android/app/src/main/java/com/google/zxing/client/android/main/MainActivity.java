a7bb75a920369b96df914a64f3a0196f91a2fd42
Limky <limky@sqisoft.com>
2016-09-19 18:17:29 +0900

    first commit

---
+++ Zxing code version-01/android/app/src/main/java/com/google/zxing/client/android/main/MainActivity.java
@@ -0,0 +1,52 @@
+package com.google.zxing.client.android.main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.client.android.CaptureActivity;
+import com.google.zxing.client.android.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button ScanButton , ListButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ScanButton = (Button)findViewById(R.id.ScanButton);
+        ListButton = (Button)findViewById(R.id.ListButton);
+
+
+        ScanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"스캔 버튼 클릭.",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        CaptureActivity.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        });
+
+
+
+        ListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"리스트 버튼 클릭.",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+    }
+
+
+}
